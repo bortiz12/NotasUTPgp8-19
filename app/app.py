2fba4f9e1100fb1d1819849017f449ccc7d473d7
Bortiz12 <brayanortix1@gmail.com>
2021-10-12 17:06:14 -0500

    gestion users

--- app/app.py
+++ app/app.py
@@ -10,10 +10,16 @@ def index():
 def home():
     return render_template('home.html')
 
+@app.route("/Gestionusu")
+def Gestionusu():
+    return render_template('Gestionusu.html')
+
 @app.errorhandler(404)
 def pagina_no_encontrada(e):
     return redirect(url_for('index'))
     
+
+
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
     
