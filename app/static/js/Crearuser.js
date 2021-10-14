cc5e427993e5b60e06e8eef177d61476672d12aa
Bryan Ortiz <brayanortix1@gmail.com>
2021-10-13 22:56:19 -0500

    ventana modal registro usuarios

---
+++ app/static/js/Crearuser.js
@@ -0,0 +1,4 @@
+function Pasarvalores(){
+    document.getElementById("inputUser").value=document.getElementById("inputCed").value;
+    document.getElementById("inputpassword").value=document.getElementById("inputCed").value;
+    }
